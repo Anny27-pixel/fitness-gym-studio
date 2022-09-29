5f52353be6b4297cbda7ce6689c24606bc8c205f
Anny27-pixel <jabintasmin102523@gmail.com>
2022-09-29 13:52:35 +0600

    added personal detail and styled it

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,30 @@
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
+import './List.css';
 const List = ({ list }) => {
     return (
-        <div>
-            <h3>my profile</h3>
+        <div className='list'>
+            <div className='profile'>
+                <h3>Jabin Tasmin Anny</h3>
+                <p className='profile-info'>  <FontAwesomeIcon icon={faLocation}></FontAwesomeIcon>
+                    <span>HSTU, Dinajpur</span>
+                </p>
+                <div className='personal-details'>
+                    <div>
+                        <p className='large-text'>46 <span>kg</span></p>
+                        <p>Weight</p>
+                    </div>
+                    <div>
+                        <p className='large-text'>4.10</p>
+                        <p>Height</p>
+                    </div>
+                    <div>
+                        <p className='large-text'>22 <span>years</span></p>
+                        <p>Age</p>
+                    </div>
+                </div>
+            </div>
             <p>selected exercise: {list.length}</p>
         </div>
     );
