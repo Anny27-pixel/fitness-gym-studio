517f3de4f08e3d82373ea3551d909fca8f2e0e7d
Anny27-pixel <jabintasmin102523@gmail.com>
2022-09-29 14:32:25 +0600

    break time added and styled

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,8 @@
 import { faLocation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import './List.css';
 const List = ({ list }) => {
-    console.log(list);
+    // console.log(list);
     let total = 0;
     for (const exercise of list) {
         total = total + exercise.time;
@@ -31,13 +30,26 @@ const List = ({ list }) => {
                 </div>
             </div>
             <h3>Add a Break</h3>
-            <div>
-                <button>10</button>
+            <div className='break-time'>
+                <div className='single-time'>
+                    <button>10 <span>s</span></button>
+                </div>
+                <div className='single-time'>
+                    <button>20 <span>s</span></button>
+                </div>
+                <div className='single-time'>
+                    <button>25 <span>s</span></button>
+                </div>
+                <div className='single-time'>
+                    <button>30 <span>s</span></button>
+                </div>
+                <div className='single-time'>
+                    <button>40 <span>s</span></button>
+                </div>
             </div>
             <h3>Exercise Details</h3>
             <p className='exercise-detail'>Exercise Time :{total}s </p>
-            <p>Break Time: </p>
-            <p>selected exercise: {list.length}</p>
+            <p className='exercise-detail'>Break Time: </p>
         </div>
     );
 };
